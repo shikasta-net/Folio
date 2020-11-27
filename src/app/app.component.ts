917107e8fd4c97db3f1554f1119fe7e863288d01
Kym Eden <shikasta.net@gmail.com>
2020-11-27 15:20:32 +0000

    Improve encapsulation and performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AppComponent {
   title = 'Folio';
